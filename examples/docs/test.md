2e9129bae9c1e4ceaab88be33883d6884680302d
xianpengfei <xianpengfei@bailuntec.com>
2020-10-24 18:02:02 +0800

    初步完成button

--- examples/docs/test.md
+++ examples/docs/test.md
@@ -3,7 +3,7 @@
 测试组件是用来测试md文档是否能在项目中像vue文件一样正常展示代码运行结果
 
 运行结果如下
-<su-button bgColor="red"></su-button>
+<su-button bgColor="red">11111111111111</su-button>
 
 代码如下
 
--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -1,33 +1,120 @@
 <template>
-    <div class="zm-test" :style="{backgroundColor: bgColor}"></div>
+    <div :class="['su-button',round?'su-button-round':'',type?'su-button-'+type:'',animation&&type?'su-button-'+type+'-'+animation:'']">
+      <slot></slot>
+    </div>
 </template>
  
 <script>
     export default {
-        name: 'SuButton',
-        props: {
-            bgColor: {
-                type: String,
-                default: "#ccc"
-            }
+      name: 'SuButton',
+      props: {
+        round:{
+          type:Boolean,
+          default:false
+        },
+        type:{
+          type:String,
+          default:"primary"
+        },
+        animation:{
+          type:String,
+          default:"criss-cross"
+        },
+        bgColor:{
+          type:String,
+          default:'#707AE4'
         }
+      }
     }
 </script>
  
-<style lang="less">
-    .zm-test {
-        display: inline-block;
+<style lang="less" scoped>
+@import url('../../style/index.less');
+.su-button {
+  position: relative;
+  display: inline-block;
+  padding: 2px 10px;
+  &-primary{
+    border: 1px solid @primaryColor;
+    box-sizing: border-box;
+    color: @primaryColor;
+    background: @primary-color-rgba;
+    overflow: hidden;
+    transition: all 1s;
+    &:hover{
+      color: #fff;
+    }
+    &-criss-cross {
+      position: relative;
+      &::before {
+        transform: translate(-50%, -50%);
+        z-index: -1;
+        left: -20px;
+        position: absolute;
+        top: 50%;
+        content: '';
         width: 10px;
         height: 10px;
+        background: @primaryColor;
         border-radius: 50%;
+      }
+      &::after{
+        transform: translate(50%, -50%);
+        z-index: -1;
+        right: -20px;
+        position: absolute;
+        top: 50%;
+        content: '';
+        width: 10px;
+        height: 10px;
+        background: @primaryColor;
+        border-radius: 50%;
+      }
+      &:hover{
+        &::after{
+          animation: criss-cross-right 0.8s both;
+        }
+        &::before{
+          animation: criss-cross-left 0.8s both;
+        }
+      }
     }
-    @keyframes zm-opacity{
-        0% {opacity:1}
-        10%,90%{opacity:.8}
-        20%,80%{opacity:.7}
-        30%,70%{opacity:.5}
-        40%,60%{opacity:.3}
-        50%{opacity:0}
-        100%{opacity:.95}
-    }
+  }
+  
+  
+  
+  &-round{
+    border-radius: 32px;
+  }
+}
+@keyframes criss-cross-left {
+  0% {
+    left: -20px;
+  }
+  50% {
+    left: 50%;
+    width: 20px;
+    height: 20px;
+  }
+  100% {
+    left: 50%;
+    width: 375px;
+    height: 375px;
+  }
+}
+@keyframes criss-cross-right {
+  0% {
+    right: -20px;
+  }
+  50% {
+    right: 50%;
+    width: 20px;
+    height: 20px;
+  }
+  100% {
+    right: 50%;
+    width: 375px;
+    height: 375px;
+  }
+}
 </style>

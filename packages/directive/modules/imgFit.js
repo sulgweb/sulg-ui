067be17d618274202a4da32295e13f695eb8faa9
xianpengfei <xianpengfei@bailuntec.com>
2021-03-24 10:43:25 +0800

    update

--- packages/directive/modules/imgFit.js
+++ packages/directive/modules/imgFit.js
@@ -2,10 +2,10 @@
  * @description: 
  * @author: 小羽
  * @Date: 2021-03-01 09:33:59
- * @LastEditTime: 2021-03-01 09:54:49
+ * @LastEditTime: 2021-03-24 10:43:08
  * @Copyright: 1.0.0
  */
-export default{
+export default {
   suImgFit:{
     inserted(el,building){
       el.style["object-fit"]= "cover"
